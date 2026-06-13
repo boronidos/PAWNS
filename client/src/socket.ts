ad07e53618c6cc9067442c7c6f306f3f4319cb00
daw123 <itesim2009lol@gmail.com>
2026-06-13 15:32:14 +0200

    changed socket url

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,4 +1,8 @@
 // src/socket.ts
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+const PRODUCTION_URL = "https://pawns-6b0v.onrender.com"; 
+
+export const socket = io(PRODUCTION_URL, {
+  transports: ["websocket", "polling"]
+});
