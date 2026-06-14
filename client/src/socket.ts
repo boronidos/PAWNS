80d5af7a19b830f3161213bec954a7948261bd3f
daw123 <itesim2009lol@gmail.com>
2026-06-14 03:25:01 +0200

    fixed socket

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,7 +1,7 @@
 // src/socket.ts
 import { io } from "socket.io-client";
 
-const PRODUCTION_URL = "localhost:3000" //"https://pawns-6b0v.onrender.com"; 
+const PRODUCTION_URL = "https://pawns-6b0v.onrender.com"; 
 
 export const socket = io(PRODUCTION_URL, {
   transports: ["websocket", "polling"]
